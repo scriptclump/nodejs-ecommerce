c0e9099ff7f3c37bdd77c66a5d43195bd5d910d7
Basant Kumar Sharma <scriptclump@gmail.com>
2020-04-20 19:19:33 +0530

    Adds the product routes to global route

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Ecommerce Boilerplate NodeJS Application
 RESTful API of e-commerce application using NodeJS
+
+## Endpoints
+- /api/category
+- /api/product
--- app/routes/index.js
+++ app/routes/index.js
@@ -9,4 +9,5 @@ APP.get("/", (req, res) => {
 // routes
 require('./auth.route');
 require('./user.route');
-require('./category.route')
+require('./category.route');
+require('./product.route')
